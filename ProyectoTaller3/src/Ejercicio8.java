51194ac29a477f589bb7a3bbae50d04184f7c239
rjr16 <rickyrjrt@gmail.com>
2024-10-24 18:26:01 -0500

    terminado

--- ProyectoTaller3/src/Ejercicio8.java
+++ ProyectoTaller3/src/Ejercicio8.java
@@ -6,5 +6,7 @@ public class Ejercicio8 {
         boolean bool = (0 !=numerador3);
         respuesta8 = (numerador3 !=0) && (Math.pow(3, 2)>= 200) || (50 >= 50 - 50 + 51);
         System.out.println("respuesta8 = " + respuesta8);
+        //Fue editado el ejercicio puesto que no deja realizarlo al tomar valores en el primer termino como int y el segundo como double
+        //agrege un and en ves de el + del ejercicio para que pueda realizarse de manera correcta 
     }
 }
