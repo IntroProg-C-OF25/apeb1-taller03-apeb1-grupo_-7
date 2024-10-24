e824dbfb15bdd521c0a870d4436de0c84f265478
rjr16 <rickyrjrt@gmail.com>
2024-10-24 16:37:18 -0500

    nueva actualizacion

--- ProyectoTaller3/src/Ejercicio7.java
+++ ProyectoTaller3/src/Ejercicio7.java
@@ -4,7 +4,10 @@ public class Ejercicio7 {
         boolean respuesta7;
         double numerador = ((Math.sqrt(81)+9)/3);
         double numerador2 = ((100)/25 + Math.sqrt(100));
-        respuesta7 = (numerador ==9) ||(10>1);
+        
+        boolean bool = (0 != numerador2);
+        
+        respuesta7 = (numerador ==9) ||(10>1) && (numerador2 != 0);
         System.out.println("respuesta7 = " + respuesta7); 
     }
 }
