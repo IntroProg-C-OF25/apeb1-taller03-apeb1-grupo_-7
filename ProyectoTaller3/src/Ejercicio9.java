b97f6c26ec9dad8c2bf6a26db99096dd18bf4526
rjr16 <rickyrjrt@gmail.com>
2024-10-24 18:31:25 -0500

    Taller completado

--- ProyectoTaller3/src/Ejercicio9.java
+++ ProyectoTaller3/src/Ejercicio9.java
@@ -1,4 +1,15 @@
 
 public class Ejercicio9 {
-    
+
+    public static void main(String[] args) {
+        double respuesta8;
+        respuesta8 = (Math.sqrt(81) + Math.sqrt(25) * 10);
+        if (respuesta8 >= 50) {
+            System.out.println("true:" + respuesta8 + " >= 50");
+        } else {
+            System.out.println("respuesta8 = " + respuesta8);
+
+        }
+    }
 }
+
