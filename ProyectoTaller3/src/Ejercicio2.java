55a007978b7dfb897e92c5e2296eb8d46761137a
rjr16 <rickyrjrt@gmail.com>
2024-10-24 18:32:28 -0500

    al fin

--- ProyectoTaller3/src/Ejercicio2.java
+++ ProyectoTaller3/src/Ejercicio2.java
@@ -1,9 +1,10 @@
 
 public class Ejercicio2 {
+
     public static void main(String[] args) {
         double respuesta2;
-        respuesta2 = (Math.pow (10,2))+10;
+        respuesta2 = (Math.pow(10, 2)) + 10;
         System.out.println("respuesta = " + respuesta2);
-        
+
     }
 }
--- ProyectoTaller3/src/Ejericicio1.java
+++ ProyectoTaller3/src/Ejericicio1.java
@@ -1,8 +1,10 @@
+
 public class Ejericicio1 {
-       public static void main(String[] args) {
+
+    public static void main(String[] args) {
         double respuesta1;
-        respuesta1 = ((1+2+4+3)/5) + Math.sqrt(81);
+        respuesta1 = ((1 + 2 + 4 + 3) / 5) + Math.sqrt(81);
         System.out.println("respuesta = " + respuesta1);
-              
-       }
+
+    }
 }
