937fdf6480a14ab30500f22495fc81dee1184432
rjr16 <rickyrjrt@gmail.com>
2024-10-24 17:46:36 -0500

    ejercicio8

--- ProyectoTaller3/src/Ejercicio8.java
+++ ProyectoTaller3/src/Ejercicio8.java
@@ -2,8 +2,9 @@
 public class Ejercicio8 {
     public static void main(String[] args) {
         boolean respuesta8;
-        
-        respuesta8 = (((10*9)+20-(500)/2)+(Math.pow(3, 2)>= 200)) || (50 >= 50 - 50 + 51);
+        int numerador3 = ((10*9)+20-(500)/2);
+        boolean bool = (0 !=numerador3);
+        respuesta8 = (numerador3 !=0) && (Math.pow(3, 2)>= 200) || (50 >= 50 - 50 + 51);
         System.out.println("respuesta8 = " + respuesta8);
     }
 }
